b9c4464942f0470b9aae526a9b65d816a565f0aa
keyforce97 <56835135+keyforce97@users.noreply.github.com>
2025-06-23 22:43:59 -0400

    Update redis_client.py

--- App/redis_client.py
+++ App/redis_client.py
@@ -1,6 +1,6 @@
 import os
 import redis
 
-# Connexion Redis via l'URL d'environnement
-REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost')
+REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
 redis_client = redis.Redis.from_url(REDIS_URL)
+
